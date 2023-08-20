0cbeb6f1a544c5c3d9a116c1c35b18ca44a758c4
ShaikHaseenaBegum <141468063+ShaikHaseenaBegum@users.noreply.github.com>
2023-08-20 23:09:31 +0530

    Add files via upload

---
+++ ConvertingForienHeatToCelcius.java
@@ -0,0 +1,26 @@
+package internsavy_task01;
+
+public class ConvertingForienHeatToCelcius {
+	  // Method to convert Foreignheat to Celcius
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    // Method to convert Celcius to Foreignheat
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static void main(String[] args) {
+        // Test the conversion methods
+        double fahrenheitValue = 77.0;
+        double celsiusValue = 25.0;
+
+        double convertedCelsius = fahrenheitToCelsius(fahrenheitValue);
+        double convertedFahrenheit = celsiusToFahrenheit(celsiusValue);
+
+        System.out.println(fahrenheitValue + " degrees Fahrenheit is equal to " + convertedCelsius + " degrees Celsius.");
+        System.out.println(celsiusValue + " degrees Celsius is equal to " + convertedFahrenheit + " degrees Fahrenheit.");
+    }
+}
+
